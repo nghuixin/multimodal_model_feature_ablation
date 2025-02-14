dc6717c4ab14634ee1b43d84c323896bf202b97c
Renee (Hui Xin) Ng <nghuixin@hotmail.com>
2025-02-13 22:03:46 -0800

    Add files via upload

---
+++ imaging_features.py
@@ -0,0 +1,105 @@
+# Lists for each imaging modality
+hyperintensities_flair = [
+    'Left Deep White Matter Hyperintensity volume (mm3) FLAIR',
+    'Right Deep White Matter Hyperintensity volume (mm3) FLAIR',
+    'Left Periventricular Hyperintensity volume (mm3) FLAIR',
+    'Right periventricular Hyperintensity volume (mm3) FLAIR'
+]
+
+dti = [
+    'Right cingulum hippocampus FA non zero mean -JHU',
+    'Left cingulum hippocampus FA non zero mean -JHU',
+    'Corpus Callosum body FA mean TBSS',
+    'Corpus Callosum Genu FA mean TBSS',
+    'Corpus Callosum Splenium FA mean TBSS',
+    'Left superior longitudinal fasciculus FA mean TBSS',
+    'Right superior longitudinal fasciculus FA mean TBSS',
+    'Left Uncinate Fasciculus FA mean TBSS',
+    'Right Uncinate Fasciculus FA mean TBSS'
+]
+
+cbf = [
+    'NZ mean L hippocampus CBF',
+    'NZ mean R hippocampus CBF',
+    'NZ mean L parahippocampal CBF',
+    'NZ mean R parahippocampal CBF',
+    'Left medial inferior frontal lobe CBF',
+    'Right medial inferior frontal lobe CBF',
+    'Left superiolateral frontal lobe CBF',
+    'Right superiolateral frontal lobe CBF',
+    'Left lateral parietal lobe CBF',
+    'Right lateral parietal lobe CBF',
+    'left medial parietal lobe CBF',
+    'Right medial parietal lobe CBF',
+    'Left frontal Lobe CBF',
+    'Right frontal Lobe CBF',
+    'Left parietal Lobe CBF',
+    'Right parietal Lobe CBF',
+    'Left hemisphere CBF',
+    'Right hemisphere CBF',
+    'Whole Brain Gray Matter CBF nz mean'
+]
+
+cortical_thickness = [
+    'Left medial inferior frontal lobe cortical thickness',
+    'Right medial inferior frontal lobe cortical thickness',
+    'Left superiolateral frontal lobe cortical thickness',
+    'Right superiolateral frontal lobe cortical thickness',
+    'Left medial parietal lobe cortical thickness',
+    'Right medial parietal lobe cortical thickness',
+    'Left lateral parietal lobe cortical thickness',
+    'Right lateral parietal lobe cortical thickness',
+    'Left frontal lobe average cortical thickness',
+    'Right frontal lobe average cortical thickness',
+    'Left parietal lobe average cortical thickness',
+    'Right parietal lobe average cortical thickness',
+    'total left hemisphere average cortical thickness',
+    'total right hemisphere average cortical thickness',
+    'total whole brain average cortical thickness'
+]
+
+surface_area = [
+    'Left medial inferior frontal lobe surface area',
+    'Right medial inferior frontal lobe surface area',
+    'Left superiolateral frontal lobe surface area',
+    'Right superiolateral frontal lobe surface area',
+    'Left medial parietal lobe surface area',
+    'Right medial parietal lobe surface area',
+    'Left lateral parietal lobe surface area',
+    'Right lateral parietal lobe surface area',
+    'Left frontal lobe surface area',
+    'Right frontal lobe surface area',
+    'Left parietal lobe surface area',
+    'Right parietal lobe surface area',
+    'Total Left hemisphere surface area',
+    'Total right hemisphere surface area',
+    'total whole brain surface area'
+]
+
+ventricular_volume = [
+    'Left_Lateral_Ventricle V',
+    'Right_Lateral_Ventricle V',
+    'Left_Inf_Lat_Vent V',
+    'Right_Inf_Lat_Vent V',
+    'parahippocampal_Left_V',
+    'parahippocampal_Right_V',
+    'Left_Hippocampus V',
+    'Right_Hippocampus V',
+    'Left hippocampal and parahippocampal volume',
+    'Right hippocampal and parahippocampal volume',
+    'total left hemisphere gray matter volume',
+    'total right hemisphere gray matter volume',
+    'total whole brain gray matter volume'
+]
+resting_state_activation = [ #BOLD
+    'DMN resting state Z correlation', #rs
+    'Task positive network Z correlation', #rs
+    '2v1 right anterior cingulate deactivation', #n-beck
+    '2v1 right parietal lobule activation', #n-beck
+    '2v1 left middle frontal gyrus activation', #n-beck
+    '2v1 right middle frontal gyrus activation', #n-beck
+    'Left middle frontal gyrus DMS activation', #delayed match to sample
+    'Right middle frontal gyrus DMS activation',
+    'Left supramarginal (parietal) gyrus DMS activation',
+    'Right supramarginal (parietal) gyrus DMS activation'
+]
